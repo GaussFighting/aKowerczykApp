d7ac96ed8e695880580e1e2731417422a3f5b495
Gauss <krzysztof.kowerczyk@gmail.com>
2021-11-03 19:11:59 +0100

    navi sketch

---
+++ src/components/Navigation.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
+
+
+
+function Navigation() {
+    return (
+        <div>
+
+            <Navbar
+                color="success"
+                expand="md"
+                fixed="top"
+                full
+                light
+            >
+                <NavbarBrand href="/">
+                    LOGO
+                </NavbarBrand>
+                <NavbarToggler onClick={function noRefCheck() { }} />
+                <Collapse navbar>
+                    <Nav
+                        className="justify-content-end" style={{ width: "100%" }}
+                        navbar
+                    >
+                        <NavItem >
+                            <NavLink className="pl-4" href="">
+                                STRONA GŁÓWNA
+                            </NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink className="pl-4" href="">
+                                USŁUGI
+                            </NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink className="pl-4" href="">
+                                BLOG
+                            </NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink className="pl-4" href="">
+                                OFERTY
+                            </NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink className="pl-4" href="">
+                                O NAS
+                            </NavLink>
+                        </NavItem>
+                        <NavItem >
+                            <NavLink className="pl-4" href="">
+                                KONTAKT
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+};
+
+export default Navigation;
