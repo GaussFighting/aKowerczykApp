7b350918e23193706a13d0bcf0ba1319768a9fa5
kirkol <antonina.kirkalo@gmail.com>
2021-11-08 18:19:05 +0100

    form for Netlify

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,14 +6,6 @@ function MainPage() {
     return (
         <div>
             <MPAbout/>
-            <div style={{backgroundColor: "#f3f2f2", height: "200px"}}></div>
-            <div style={{backgroundColor: "rgb(200, 200, 200)", height: "200px"}}></div>
-            <div style={{backgroundColor: "#f3f2f2", height: "200px"}}></div>
-            <div style={{backgroundColor: "rgb(200, 200, 200)", height: "200px"}}></div>
-            <div style={{backgroundColor: "#f3f2f2", height: "200px"}}></div>
-            <div style={{backgroundColor: "rgb(200, 200, 200)", height: "200px"}}></div>
-            <div style={{backgroundColor: "#f3f2f2", height: "200px"}}></div>
-            <div style={{backgroundColor: "rgb(200, 200, 200)", height: "200px"}}></div>
             <MPContact/>
         </div>
     )
