0b037704cf0b7a63217fbcd3cda51efccf709525
Gauss <krzysztof.kowerczyk@gmail.com>
2021-11-13 11:59:05 +0100

    offers sketch

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TopImage from './TopImage';
 import MPContact from './MPContact';
 import MPAbout from './MPAbout';
+import MPOffers from './MPOffers';
 import MPServices from './MPServices';
 import MPUsefulLinks from "./MPUsefulLinks"
 import MPNews from './MPNews';
@@ -13,6 +14,7 @@ function MainPage() {
             <TopImage image={mainPageImage} position={78}/>
             <MPAbout/>
             <MPServices/>
+            <MPOffers />
             <MPNews/>
             <MPUsefulLinks/>
             <MPContact/>
--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TopImage from './TopImage';
+import Offer from './Offer';
 import offersPageImage from '../styles/images/interior-design-g8903eca74_1920.jpg';
+import offers from '../data/offers.json';
 
 function Offers() {
   return (
@@ -8,6 +10,8 @@ function Offers() {
       <TopImage image={offersPageImage} position={68} />
       <div className="container page-container py-5">
         <h1>OFERTY</h1>
+        <Offer  />
+       
       </div>
     </div>
   );
